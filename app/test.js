ecaf0d37b5ed436c53b3a4057c1944778d5d3906
Jason Hui <jason903813@gmail.com>
2024-06-24 23:57:06 +0100

    add range selecetor

--- app/test.js
+++ app/test.js
@@ -1,10 +1,11 @@
 import React, { useState, useCallback } from 'react';
 import { FaCheck, FaArrowUp, FaArrowDown } from 'react-icons/fa';
 import { addHistory } from './Storage/test_history';
+import { getRandomInt } from './utils';
 
 const colorOptions = [
     { value: 'all', label: 'All Colors', range: [0, 360] },
-    { value: 'red', label: 'Red', range: [[0, 15], [330, 360]] },
+    { value: 'red', label: 'Red', range: [330, 375] },
     { value: 'orange', label: 'Orange', range: [15, 45] },
     { value: 'yellow', label: 'Yellow', range: [45, 75] },
     { value: 'green', label: 'Green', range: [75, 150] },
@@ -12,36 +13,49 @@ const colorOptions = [
     { value: 'blue', label: 'Blue', range: [210, 240] },
     { value: 'indigo', label: 'Indigo', range: [240, 270] },
     { value: 'purple', label: 'Purple', range: [270, 330] },
+    { value: 'custom', label: 'Custom', range: [] },
 ];
 
+const brightnessOptions = [
+    { value: 'all', label: 'All Brightness', range: [0, 100] },
+    { value: 'very bright', label: 'Very Bright', range: [75, 97] },
+    { value: 'bright', label: 'Bright', range: [50, 75] },
+    { value: 'dim', label: 'Dim', range: [25, 50] },
+    { value: 'very dim', label: 'Very Dim', range: [10, 25] },
+    { value: 'custom', label: 'Custom', range: [] },
+
+];
+
+const saturationOptions = [
+    { value: 'all', label: 'All Saturation', range: [0, 100] },
+    { value: 'very saturated', label: 'Very Saturated', range: [75, 100] },
+    { value: 'saturated', label: 'Saturated', range: [50, 75] },
+    { value: 'less saturated', label: 'Less Saturated', range: [25, 50] },
+    { value: 'desaturated', label: 'Desaturated', range: [0, 25] },
+    { value: 'custom', label: 'Custom', range: [] },
+];
+
+function generateRandomColor(h_range, s_range, v_range) {
+    // allow wrapping
+    const h = getRandomInt(h_range[0], h_range[1]) % 361;
+    const s = getRandomInt(s_range[0], s_range[1]) % 101;
+    const v = getRandomInt(v_range[0], v_range[1]) % 101;
+
+    return { h, s, v };
+}
+
 const ColorTest = ({ selectedColor, targetColor, setTargetColor }) => {
-    const [colorRange, setColorRange] = useState('all');
+    const [hSelected, setHSelected] = useState('all');
+    const [sSelected, setSSelected] = useState('all');
+    const [vSelected, setVSelected] = useState('all');
     const [showResult, setShowResult] = useState(false);
 
-    const generateRandomColor = useCallback(() => {
-        let h, s, v;
-
-        const selectedColor = colorOptions.find(option => option.value === colorRange);
-
-        if (Array.isArray(selectedColor.range[0])) {
-            //red
-            const randomIndex = Math.floor(Math.random() * selectedColor.range.length);
-            const [start, end] = selectedColor.range[randomIndex];
-            h = Math.floor(Math.random() * (end - start)) + start;
-        } else {
-            const [start, end] = selectedColor.range;
-            h = Math.floor(Math.random() * (end - start)) + start;
-        }
-
-        s = Math.floor(Math.random() * 101); // Saturation between 0 and 100
-        v = Math.floor(Math.random() * 90 + 5); // Value between 5 and 95
-        // v = Math.floor(Math.random() * 101); // Value between 0 and 100
-
-        return { h, s, v };
-    }, [colorRange]);
+    const [hRange, setHRange] = useState([0, 360]);
+    const [sRange, setSRange] = useState([0, 100]);
+    const [vRange, setVRange] = useState([0, 100]);
 
     const startTest = () => {
-        const newTargetColor = generateRandomColor();
+        const newTargetColor = generateRandomColor(hRange, sRange, vRange);
         setTargetColor(newTargetColor);
         setShowResult(false);
     };
@@ -72,24 +86,28 @@ const ColorTest = ({ selectedColor, targetColor, setTargetColor }) => {
 
         };
     };
-
     return (
         <div>
             <div className="mt-6">
                 <h3 className="text-lg font-semibold mb-2">Color Test</h3>
-                <div className="flex items-center space-x-4 mb-4">
-                    <label className="font-medium">Color Range:</label>
-                    <select
-                        value={colorRange}
-                        onChange={(e) => setColorRange(e.target.value)}
-                        className="border rounded px-2 py-1"
-                    >
-                        {colorOptions.map((option) => (
-                            <option key={option.value} value={option.value}>
-                                {option.label}
-                            </option>
-                        ))}
-                    </select>
+
+                <div className='flex flex-col gap-4 mb-4'>
+
+                    <div className='flex gap-4 items-center  justify-between'>
+                        <RangeSelection current={hSelected} setCurrent={setHSelected} setRange={setHRange} options={colorOptions} label={'H'} />
+                        <CustomRangeSelector range={hRange} setCustomRange={setHRange} min={0} max={720} onChange={() => setHSelected('custom')} />
+                    </div>
+
+
+                    <div className='flex gap-4 items-center justify-between'>
+                        <RangeSelection current={vSelected} setCurrent={setVSelected} setRange={setVRange} options={brightnessOptions} label={'L'} />
+                        <CustomRangeSelector range={vRange} setCustomRange={setVRange} min={0} max={200} onChange={() => setVSelected('custom')} />
+                    </div>
+
+                    <div className='flex gap-4 items-center justify-between'>
+                        <RangeSelection current={sSelected} setCurrent={setSSelected} setRange={setSRange} options={saturationOptions} label={'S'} />
+                        <CustomRangeSelector range={sRange} setCustomRange={setSRange} min={0} max={200} onChange={() => setSSelected('custom')} />
+                    </div>
                 </div>
                 <button
                     onClick={startTest}
@@ -124,6 +142,57 @@ const ColorTest = ({ selectedColor, targetColor, setTargetColor }) => {
         </div>
     );
 }
+
+const RangeSelection = ({ current, setCurrent, setRange, options, label }) => {
+    return (
+        <div className="flex items-center space-x-4">
+            <label className="font-medium">{label}:</label>
+            <select
+                value={current}
+                onChange={(e) => {
+                    setCurrent(e.target.value);
+                    if (e.target.value === 'custom') return
+                    setRange(options.find((option) => option.value === e.target.value).range)
+                }}
+                className="border rounded px-2 py-1"
+            >
+                {options.map((option) => (
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+        </div>)
+}
+
+const CustomRangeSelector = ({ onChange, range, setCustomRange, min, max }) => {
+    const handleRangeChange = (index, newValue) => {
+        const updatedRange = [...range];
+        updatedRange[index] = Number(newValue);
+        setCustomRange(updatedRange);
+        onChange();
+    };
+
+    return (
+        <div>
+            <input
+                type="number"
+                value={range[0]}
+                onChange={(e) => { handleRangeChange(0, e.target.value); }}
+                min={min}
+                max={max}
+            />
+            <input
+                type="number"
+                value={range[1]}
+                onChange={(e) => handleRangeChange(1, e.target.value)}
+                min={min}
+                max={max}
+            />
+        </div>
+    );
+};
+
 
 const RenderResult = ({ type, result, difference }) => {
     return (<div className="space-y-2">
--- app/utils.js
+++ app/utils.js
@@ -41,3 +41,9 @@ export function hexToHsl(hex) {
     // Use our rgbToHsl function
     return rgbToHsl(r, g, b);
 }
+
+export function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
